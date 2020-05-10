540883bbde6584203d8d3f7c1f58505ff0c57d10
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-10 18:24:42 +0000

    Automatically backed up by Learn

--- db/migrate/20200510181947_create_dogs.rb
+++ db/migrate/20200510181947_create_dogs.rb
@@ -17,3 +17,5 @@ end
 
 #Why add SINATRA_ENV=development, you might ask? Well, remember the top line of config/environment.rb? It's telling Sinatra that it should use the "development" environment for both shotgun and the testing suite. Therefore, we want to make sure our migrations run on the same environment as well, and specifying SINATRA_ENV=development allows us to do that.
 
+#The change method is actually a shorter way of writing up and down methods. We can refactor our migration to look like this:
+
