f4cdd7de6e951c3050c4f5e3e6d88f1c1e2125f3
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-10 18:18:03 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -6,5 +6,6 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 configure :development do
   set :database, 'sqlite3:db/database.db'
 end
+#f we wanted our .db file to be called dogs.db, we could simply change the name of this file.
 
 require './app'
